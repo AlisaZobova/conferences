1871a21c1f9e9ca9bdb29b176d2b304acbad2922
Alisa Zobova <zobovaalice@gmail.com>
2022-11-06 13:23:34 +0200

    Change main view

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,10 +1,12 @@
 <?php include_once 'header.php'; ?>
 
-<title>Main page</title>
+<title>Conferences</title>
+
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <button class="btn btn-success mt-2" onclick="location.href='application/views/create_view.php'" data-target="#create"><i class="fa fa-plus"></i></button>
+            <button class="btn btn-success mt-2" onclick="location.href='application/views/create_view.php'"
+                    data-target="#create"><i class="fa fa-plus"></i></button>
             <table class="table table-striped table-hover mt-2">
                 <thead class="thead-dark">
                 <th>Title</th>
@@ -13,24 +15,28 @@
                 </thead>
                 <tbody>
                 <?php foreach ($data as $res) { ?>
-                <tr>
-                    <td><?php echo $res->title; ?></td>
-                    <td><?php echo $res->conf_date; ?></td>
-                    <td>
-                        <a href="?edit=true&conference_id=<?php echo $res->conference_id; ?>" class="btn btn-success" data-target="#edit<?php echo $res->conference_id; ?>"><i class="fa fa-edit"></i></a>
-                        <a href="" class="btn btn-danger" data-toggle="modal" data-target="#delete<?php echo $res->conference_id; ?>"><i class="fa fa-trash-alt"></i></a>
-                        <a href="?conference_id=<?php echo $res->conference_id; ?>" class="btn btn-success"  data-target="#read<?php echo $res->conference_id; ?>"><i class="fa fa-info-circle"></i></a>
-                    </td>
-                </tr>
+                    <tr>
+                        <td><?php echo $res->title; ?></td>
+                        <td><?php echo $res->conf_date; ?></td>
+                        <td>
+                            <a href="?edit=true&conference_id=<?php echo $res->conference_id; ?>"
+                               class="btn btn-success" data-target="#edit<?php echo $res->conference_id; ?>"><i
+                                        class="fa fa-edit"></i></a>
+                            <a href="" class="btn btn-danger" data-toggle="modal"
+                               data-target="#delete<?php echo $res->conference_id; ?>"><i
+                                        class="fa fa-trash-alt"></i></a>
+                            <a href="?conference_id=<?php echo $res->conference_id; ?>" class="btn btn-success"
+                               data-target="#read<?php echo $res->conference_id; ?>"><i
+                                        class="fa fa-info-circle"></i></a>
+                        </td>
+                    </tr>
+                    <?php include_once 'delete_view.php' ?>
+                <?php } ?>
+                </tbody>
+            </table>
 
-                <?php include_once 'delete_view.php'?>
-    <?php } ?>
-    </tbody>
-    </table>
-
+        </div>
+    </div>
 </div>
-</div>
-</div>
-
 
 <?php include_once 'footer.php'; ?>
