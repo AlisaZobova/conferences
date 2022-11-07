8092ca1e6986d6a9a3fcf7c5cb48f7c640c713df
Alisa Zobova <zobovaalice@gmail.com>
2022-11-07 23:49:33 +0200

    Test connection

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 ﻿<?php ini_set('display_errors', 1);
+
+$host='eu-cdbr-west-03.cleardb.net';
+$db='heroku_3bf5f17ab1f53bb';
+$user='b188abd32e12be';
+$pass='a4a903f3';
+
+$pdo = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
+
+echo 'OK';
+
 require_once 'application/bootstrap.php';
