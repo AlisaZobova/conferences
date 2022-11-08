9519d5290bf8434fbb5885224aa7655f09f0cd90
Alisa Zobova <zobovaalice@gmail.com>
2022-11-08 08:55:13 +0200

    Change create_update.php

--- application/views/create_update.php
+++ application/views/create_update.php
@@ -1,24 +1,32 @@
+<?php $title = !$res->title ?  null : $res->title;
+$conf_date = !$res->conf_date ?  null : $res->conf_date;
+$latitude = !$res->latitude ?  null : $res->latitude;
+$longitude = !$res->longitude ?  null : $res->longitude;
+$country = !$res->country ?  null : $res->country;
+?>
+
+
 <div class="form-group">
     <small>Title</small>
     <input type="text" required="required" class="form-control" name="title" minlength="2" maxlength="255"
-           value="<?php echo $res->title; ?>" pattern="[A-Z][a-z]*(\s(([A-Z][a-z]*)|([a-z]+))|(\s[0-9]+)*)*"
+           value="<?php echo $title; ?>" pattern="[A-Z][a-z]*(\s(([A-Z][a-z]*)|([a-z]+))|(\s[0-9]+)*)*"
            title="The title must start with a capital letter. Can contain words with capital or small letters, as well as numbers.">
 </div>
 <div class="form-group">
     <small>Date</small>
     <input type="date" required="required" class="form-control" name="conf_date"
-           value="<?php echo $res->conf_date; ?>">
+           value="<?php echo $conf_date; ?>">
 </div>
 <div class="form-group">
     <small>Latitude</small>
-    <input id="latitude" type="text" class="form-control latlng" name="latitude" value="<?php echo $res->latitude; ?>"
+    <input id="latitude" type="text" class="form-control latlng" name="latitude" value="<?php echo $latitude; ?>"
            pattern="[-]?[0-9]+([.][0-9]+)?"
            title="Only whole or real numbers. If the number is real, use a dot as a decimal separator.">
 </div>
 <div class="form-group">
     <small>Longitude</small>
     <input id="longitude" type="text" class="form-control latlng" name="longitude"
-           value="<?php echo $res->longitude; ?>"
+           value="<?php echo $longitude; ?>"
            pattern="[-]?[0-9]+([.][0-9]+)?"
            title="Only whole or real numbers. If the number is real, use a dot as a decimal separator.">
 </div>
@@ -28,7 +36,7 @@
 <div class="form-group">
     <label for="countries"><small>Country</small></label>
     <select class="form-control" name="country" id="countries">
-        <option><?php echo $res->country; ?></option>
+        <option><?php echo $country; ?></option>
         <?php foreach (['Ukraine', 'USA', 'UK', 'France'] as $country) {
             if ($country != $res->country) {
                 ?>
