98a61ce33d1f423a5188bbd68ce451e4e71e9023
Alisa Zobova <zobovaalice@gmail.com>
2022-11-08 12:42:17 +0200

    Change map.js
    Change map.js
    
    Change map.js

--- js/map.js
+++ js/map.js
@@ -1,18 +1,21 @@
 function initMap() {
 
-    latitude = parseFloat(document.getElementById('latitude').value);
-    longitude = parseFloat(document.getElementById('longitude').value);
+    const lat = document.getElementById('latitude');
+    const lng = document.getElementById('longitude');
+
+    let latitude = parseFloat(lat.value);
+    let longitude = parseFloat(lng.value);
 
     if (document.getElementById('read') && (!latitude && !longitude)) {
         return;
     }
 
-    var map = new google.maps.Map(document.getElementById('googleMap'), {
+    const map = new google.maps.Map(document.getElementById('googleMap'), {
         zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
-    var myMarker;
+    let myMarker;
 
     if (latitude && longitude) {
         myMarker = new google.maps.Marker({
@@ -28,7 +31,7 @@ function initMap() {
         });
     }
 
-    google.maps.event.addListener(map, 'click', function (event) {
+    map.addListener('click', function (event) {
         myMarker.setPosition(event.latLng);
         myMarker.visible = true
         myMarker.map = map;
@@ -37,47 +40,50 @@ function initMap() {
     if (document.getElementById('read')) {
         myMarker.draggable = false;
     } else {
-        google.maps.event.addListener(myMarker, 'dragend', function (evt) {
-            document.getElementById('latitude').value = evt.latLng.lat().toFixed(3);
+        myMarker.addListener('dragend', function (evt) {
+            lat.value = evt.latLng.lat().toFixed(3);
         });
 
-        google.maps.event.addListener(myMarker, 'dragend', function (evt) {
-            document.getElementById('longitude').value = evt.latLng.lng().toFixed(3);
+        myMarker.addListener(myMarker, 'dragend', function (evt) {
+            lng.value = evt.latLng.lng().toFixed(3);
         });
     }
 
     var latlng = document.querySelectorAll('input.latlng');
 
     for (let i = 0; i < latlng.length; i++) {
-        latlng[i].addEventListener('input', function (evt) {
-
-            lat = parseFloat(document.getElementById('latitude').value);
-            lng = parseFloat(document.getElementById('longitude').value);
-
-            if (!lat || !lng) {
-                myMarker.setMap(null);
-                return
-            }
-
-            map.panTo({
-                lat: lat,
-                lng: lng
-            });
-
-            if (!myMarker.map) {
-                myMarker.setMap(map);
-            }
-
-            myMarker.visible = true
-
-            myMarker.setPosition({
-                lat: lat,
-                lng: lng
-            });
-
+        latlng[i].addEventListener('input', function () {
+            setInputValue(lat, lng, myMarker, map)
         });
-    }
+        }
 
     map.setCenter(myMarker.position);
     myMarker.setMap(map);
+}
+
+function setInputValue(lat, lng, myMarker, map) {
+
+    latitude = parseFloat(lat.value);
+    longitude = parseFloat(lng.value);
+
+    if (!latitude || !longitude) {
+        myMarker.setMap(null);
+        return
+    }
+
+    map.panTo({
+        lat: latitude,
+        lng: longitude
+    });
+
+    if (!myMarker.map) {
+        myMarker.setMap(map);
+    }
+
+    myMarker.visible = true
+
+    myMarker.setPosition({
+        lat: latitude,
+        lng: longitude
+    })
 }
