c04006b6a11eadf01a21463a596e783870f4e475
Alisa Zobova <zobovaalice@gmail.com>
2022-11-08 11:51:17 +0200

    Change regex for lat and lng

--- application/views/create_update.php
+++ application/views/create_update.php
@@ -1,30 +1,30 @@
 <div class="form-group">
     <small>Title</small>
-    <?php $title = !(isset($res)) ?  null : $res->title; ?>
+    <?php $title = !(isset($res)) ? null : $res->title; ?>
     <input type="text" required="required" class="form-control" name="title" minlength="2" maxlength="255"
            value="<?php echo $title; ?>" pattern="[A-Z][a-z]*(\s(([A-Z][a-z]*)|([a-z]+))|(\s[0-9]+)*)*"
            title="The title must start with a capital letter. Can contain words with capital or small letters, as well as numbers.">
 </div>
 <div class="form-group">
     <small>Date</small>
-    <?php $conf_date = !(isset($res)) ?  null : $res->conf_date; ?>
+    <?php $conf_date = !(isset($res)) ? null : $res->conf_date; ?>
     <input type="date" required="required" class="form-control" name="conf_date"
            value="<?php echo $conf_date; ?>">
 </div>
 <div class="form-group">
     <small>Latitude</small>
-    <?php $latitude = !(isset($res)) ?  null : $res->latitude; ?>
+    <?php $latitude = !(isset($res)) ? null : $res->latitude; ?>
     <input id="latitude" type="text" class="form-control latlng" name="latitude" value="<?php echo $latitude; ?>"
-           pattern="[-]?[0-9]+([.][0-9]+)?"
-           title="Only whole or real numbers. If the number is real, use a dot as a decimal separator.">
+           pattern="-?(\d|([1-8][0-9])(\.\d)?)|(90(\.0)?)"
+           title="Only whole or real numbers between -90 and 90. If the number is real, use a dot as a decimal separator.">
 </div>
 <div class="form-group">
     <small>Longitude</small>
-    <?php $longitude = !(isset($res)) ?  null : $res->longitude; ?>
+    <?php $longitude = !(isset($res)) ? null : $res->longitude; ?>
     <input id="longitude" type="text" class="form-control latlng" name="longitude"
            value="<?php echo $longitude; ?>"
-           pattern="[-]?[0-9]+([.][0-9]+)?"
-           title="Only whole or real numbers. If the number is real, use a dot as a decimal separator.">
+           pattern="-?(\d|([1-8][0-9])(\.\d)?)|(90(\.0)?)"
+           title="Only whole or real numbers between -90 and 90. If the number is real, use a dot as a decimal separator.">
 </div>
 <div class="form-group">
     <div id="googleMap" style="width:100%;height:400px;"></div>
@@ -32,7 +32,7 @@
 <div class="form-group">
     <label for="countries"><small>Country</small></label>
     <select class="form-control" name="country" id="countries">
-        <?php $country = !(isset($res)) ?  null : $res->country; ?>
+        <?php $country = !(isset($res)) ? null : $res->country; ?>
         <option><?php echo $country; ?></option>
         <?php foreach (['Ukraine', 'USA', 'UK', 'France'] as $country) {
             if ($country != $res->country) {
